42e62d70ddc8b3fba4a1e2271bb4b3d99e105bae
Aurelien CAZUC <acazuc@student.42.fr>
2016-03-20 12:48:20 +0100

    Added toupper & tolower

--- srcs/ft_tolower.s
+++ srcs/ft_tolower.s
@@ -15,4 +15,12 @@ global _ft_tolower
 section .text
 
 _ft_tolower:
+	cmp rdi, 65
+	jb end
+	cmp rdi, 90
+	ja end
+	add rdi, 32
+
+end:
+	mov rax, rdi
 	ret
--- srcs/ft_toupper.s
+++ srcs/ft_toupper.s
@@ -15,4 +15,12 @@ global _ft_toupper
 section .text
 
 _ft_toupper:
+	cmp rdi, 97
+	jb end
+	cmp rdi, 122
+	ja end
+	sub rdi, 32
+
+end:
+	mov rax, rdi
 	ret
